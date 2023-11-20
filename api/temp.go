2769ef91fb7036f6a676c071de0a32e03ce6a497
sk-rama <35261782+sk-rama@users.noreply.github.com>
2023-11-20 15:46:20 +0100

    Create temp.go

---
+++ api/temp.go
@@ -0,0 +1,20 @@
+// GetTitleFunc returns a func that can be used to transform a string to
+// title case.
+//
+// The supported styles are
+//
+// - "Go" (strings.Title)
+// - "AP" (see https://www.apstylebook.com/)
+// - "Chicago" (see https://www.chicagomanualofstyle.org/home.html) This is very very very very very very very very very very very very very very very very very very very very very very very very very very very very very long text
+//
+// If an unknown or empty style is provided, AP style is what you get.
+func GetTitleFunc(style string) func(s string) string {
+  switch strings.ToLower(style) {
+  case "go":
+    return strings.Title
+  case "chicago":
+    return transform.NewTitleConverter(transform.ChicagoStyle)
+  default:
+    return transform.NewTitleConverter(transform.APStyle)
+  }
+}
